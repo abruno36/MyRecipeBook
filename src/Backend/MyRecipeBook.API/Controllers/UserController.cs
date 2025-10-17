e316c5223dbc6b01a4b556c53f577b079fc4fdb8
= <=>
2025-10-17 16:13:29 -0300

    feat: adding endpoint to register new user

---
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Request;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+    public IActionResult Register(RequestRegisterUserJson request)
+    {
+        return Created();
+    }
+}
---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,6 @@
+﻿namespace MyRecipeBook.Communication.Responses;
+public class ResponseRegisterUserJson
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
