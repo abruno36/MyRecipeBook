df77cb43f56b01af16a1c789b35558d9503d2ae6
Antonio Bruno <AntonioBruno@Empresario512.onmicrosoft.com>
2025-11-09 15:34:23 -0300

    feat: Geral Controllers

--- src/Backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MyRecipeBook.API.Attributes;
 using MyRecipeBook.Application.UseCases.User.ChangePassword;
 using MyRecipeBook.Application.UseCases.User.Delete.Request;
@@ -13,6 +14,7 @@ namespace MyRecipeBook.API.Controllers;
 public class UserController : MyRecipeBookBaseController
 {
     [HttpPost]
+    [AllowAnonymous] // ✅ Registro é público
     [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
     public async Task<IActionResult> Register(
         [FromServices] IRegisterUserUseCase useCase,
@@ -24,48 +26,44 @@ public class UserController : MyRecipeBookBaseController
     }
 
     [HttpGet]
-    [ProducesResponseType(typeof(ResponseUserProfileJson), StatusCodes.Status200OK)]
     [AuthenticatedUser]
+    [ProducesResponseType(typeof(ResponseUserProfileJson), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetUserProfile([FromServices] IGetUserProfileUseCase useCase)
     {
         var result = await useCase.Execute();
-
         return Ok(result);
     }
 
     [HttpPut]
+    [AuthenticatedUser]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
-    [AuthenticatedUser]
     public async Task<IActionResult> Update(
         [FromServices] IUpdateUserUseCase useCase,
         [FromBody] RequestUpdateUserJson request)
     {
         await useCase.Execute(request);
-
         return NoContent();
     }
 
     [HttpPut("change-password")]
+    [AuthenticatedUser]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
-    [AuthenticatedUser]
     public async Task<IActionResult> ChangePassword(
         [FromServices] IChangePasswordUseCase useCase,
         [FromBody] RequestChangePasswordJson request)
     {
         await useCase.Execute(request);
-
         return NoContent();
     }
 
     [HttpDelete]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
     [AuthenticatedUser]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public async Task<IActionResult> Delete([FromServices] IRequestDeleteUserUseCase useCase)
     {
         await useCase.Execute();
-
         return NoContent();
     }
 }
